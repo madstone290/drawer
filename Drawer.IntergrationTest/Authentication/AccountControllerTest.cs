cf5022e3da66bc2895ed3fc992257bf21fe0db16
madstone290 <madstone290@gmail.com>
2022-06-17 22:05:22 +0900

    Update 테스트 타입 Theory로 변경
    Update test method type from Fact to Theory

--- Drawer.IntergrationTest/Authentication/AccountControllerTest.cs
+++ Drawer.IntergrationTest/Authentication/AccountControllerTest.cs
@@ -21,13 +21,14 @@ namespace Drawer.IntergrationTest.Authentication
             _client = apiInstance.Client;
         }
 
-        [Fact]
-        public async Task Register_Returns_Ok_With_RegisterInfo()
+        [Theory]
+        [InlineData("register_ok_email1@email.com", "password1", "user1")]
+        [InlineData("register_ok_email2@email.com", "password2", "user2")]
+        [InlineData("register_ok_email3@email.com", "password3", "user3")]
+        public async Task Register_Returns_Ok_With_RegisterInfo(string email, string password, string displayName)
         {
             // Arrange
-            string email = $"{Guid.NewGuid()}@test.com";
-            string password = Guid.NewGuid().ToString();
-            var registerModel = new RegisterModel(email, password, "TestUser");
+            var registerModel = new RegisterModel(email, password, displayName);
             
             // Act
             var response = await _client.PostAsJsonAsync("account/register", registerModel);
@@ -40,13 +41,14 @@ namespace Drawer.IntergrationTest.Authentication
             registerResult!.DisplayName.Should().Be(registerModel.DisplayName);
         }
 
-        [Fact]
-        public async Task Register_With_DuplicateEmail_Returns_BadRequest()
+        [Theory]
+        [InlineData("register_bad_email1@email.com", "password1", "user1")]
+        [InlineData("register_bad_email2@email.com", "password2", "user2")]
+        [InlineData("register_bad_email3@email.com", "password3", "user3")]
+        public async Task Register_With_DuplicateEmail_Returns_BadRequest(string email,string password, string displayName)
         {
             // Arrange
-            string email = $"{Guid.NewGuid()}@test.com";
-            string password = Guid.NewGuid().ToString();
-            var registerModel = new RegisterModel(email, password, "TestUser");
+            var registerModel = new RegisterModel(email, password, displayName);
 
             // Act
             var firstResponse = await _client.PostAsJsonAsync("account/register", registerModel);
@@ -62,13 +64,14 @@ namespace Drawer.IntergrationTest.Authentication
             secondResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
 
-        [Fact]
-        public async Task Login_With_UnconfirmedEmail_Returns_BadRequest()
+        [Theory]
+        [InlineData("login_bad_email1@email.com", "password1", "user1")]
+        [InlineData("login_bad_email2@email.com", "password2", "user2")]
+        [InlineData("login_bad_email3@email.com", "password3", "user3")]
+        public async Task Login_With_UnconfirmedEmail_Returns_BadRequest(string email, string password, string displayName)
         {
             // Arrange
-            string email = $"{Guid.NewGuid()}@test.com";
-            string password = Guid.NewGuid().ToString();
-            var registerModel = new RegisterModel(email, password, "TestUser");
+            var registerModel = new RegisterModel(email, password, displayName);
             var registerResult = await _client.PostAsJsonAsync("account/register", registerModel);
             registerResult.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             var loginModel = new LoginModel(email, password);
@@ -80,12 +83,13 @@ namespace Drawer.IntergrationTest.Authentication
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
 
-        [Fact]
-        public async Task Login_With_ConfirmedEmail_Returns_Ok_With_Tokens()
+        
+        [Theory]
+        // 이메일 인증 테스트가 어렵기 때문에 마스터 계정을 이용한다
+        [InlineData("master@master.com", "master")]
+        public async Task Login_With_ConfirmedEmail_Returns_Ok_With_Tokens(string email, string password)
         {
             // Arrange
-            string email = "master@master.com";
-            string password = "master";
             var loginModel = new LoginModel(email, password);
 
             // Act
