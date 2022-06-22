c30513cc495672b5ed92e223a11815e6f65a2b26
madstone290 <madstone290@gmail.com>
2022-06-22 12:01:43 +0900

    test

--- Drawer.WebClient/Authentication/IAuthenticationManager.cs
+++ Drawer.WebClient/Authentication/IAuthenticationManager.cs
@@ -1,9 +1,21 @@
 ﻿namespace Drawer.WebClient.Authentication
 {
+    /// <summary>
+    /// 인증 기능을 제공한다.
+    /// </summary>
     public interface IAuthenticationManager
     {
-        void Login();
-
-        void Logout();
+        /// <summary>
+        /// 로그인을 진행한다.
+        /// </summary>
+        /// <returns></returns>
+        Task<AuthenticationResult> LoginAsync(string email, string password);
+        
+        /// <summary>
+        /// 로그아웃을 진행한다.
+        /// </summary>
+        /// <returns></returns>
+        Task<AuthenticationResult> LogoutAsync();
+        
     }
 }
--- Drawer.WebClient/Pages/Account/LoginHandler.cshtml.cs
+++ Drawer.WebClient/Pages/Account/LoginHandler.cshtml.cs
@@ -1,10 +1,9 @@
 using Drawer.Contract.Authentication;
 using Drawer.Contract.Common;
+using Drawer.WebClient.Authentication;
 using Drawer.WebClient.Utils;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Security.Claims;
@@ -13,50 +12,70 @@ namespace Drawer.WebClient.Pages.Account
 {
     public class LoginHandlerModel : PageModel
     {
-		private readonly HttpClient _httpClient;
-		private readonly ProtectedLocalStorage _localStorage;
+        private readonly IAuthenticationManager _authenticationManager;
 
-        public LoginHandlerModel(HttpClient httpClient, ProtectedLocalStorage localStorage)
+        public LoginHandlerModel(IAuthenticationManager authenticationManager)
         {
-            _httpClient = httpClient;
-            _localStorage = localStorage;
+            _authenticationManager = authenticationManager;
         }
 
         public async Task<IActionResult> OnGetAsync(string email, string password)
         {
+<<<<<<< Updated upstream
 			var loginResponseMessage = await _httpClient.PostAsJsonAsync("/api/account/login", new LoginRequest(email, password));
 			if (!loginResponseMessage.IsSuccessStatusCode)
 			{
 				var error = await loginResponseMessage.Content.ReadFromJsonAsync<ErrorResponse>();
 				if(error!.Code == ErrorCodes.UnconfirmedEmail)
+=======
+            var loginResult = await _authenticationManager.LoginAsync(email, password);
+            if (loginResult.IsSuccessful)
+            {
+                return Redirect(Paths.Base);
+            }
+            else if (loginResult.UnconfirmedEmail)
+            {
+                return Redirect(Paths.Account.ConfirmEmail.AddQueryParam("email", email));
+            }
+            else
+            {
+                return Redirect(Paths.Account.Login.AddQueryParam("error", loginResult!.Message));
+            }
+            
+            var loginResponseMessage = await _httpClient.PostAsJsonAsync("/api/account/login", new LoginRequest(email, password));
+            if (!loginResponseMessage.IsSuccessStatusCode)
+            {
+                var error = await loginResponseMessage.Content.ReadFromJsonAsync<ErrorResponse>();
+                if (error!.Code == ErrorCodes.NotConfirmedEmail)
+>>>>>>> Stashed changes
                 {
-					return Redirect(Paths.Account.ConfirmEmail.AddQueryParam("email", email));
+                    return Redirect(Paths.Account.ConfirmEmail.AddQueryParam("email", email));
                 }
                 else
                 {
-					return Redirect(Paths.Account.Login.AddQueryParam("error", error!.Message));
-				}
-			}
-			
-			var loginResponse = await loginResponseMessage.Content.ReadFromJsonAsync<LoginResponse>();
+                    return Redirect(Paths.Account.Login.AddQueryParam("error", error!.Message));
+                }
+            }
 
-			var claims = new List<Claim>();
-			claims.Add(new Claim(ClaimTypes.Email, email));
-			claims.Add(new Claim("AccessToken", loginResponse!.AccessToken));
-			claims.Add(new Claim("RefreshToken", loginResponse!.RefreshToken));
+            var loginResponse = await loginResponseMessage.Content.ReadFromJsonAsync<LoginResponse>();
 
-			var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-			var authProperties = new AuthenticationProperties();
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Email, email));
+            claims.Add(new Claim("AccessToken", loginResponse!.AccessToken));
+            claims.Add(new Claim("RefreshToken", loginResponse!.RefreshToken));
 
-			await HttpContext.SignInAsync(
-			   CookieAuthenticationDefaults.AuthenticationScheme,
-			   new ClaimsPrincipal(claimsIdentity),
-			   authProperties);
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties();
 
-			return Redirect(Paths.Base);
-		}
+            await HttpContext.SignInAsync(
+               CookieAuthenticationDefaults.AuthenticationScheme,
+               new ClaimsPrincipal(claimsIdentity),
+               authProperties);
+
+            return Redirect(Paths.Base);
+        }
 
 
 
-	}
+    }
 }
