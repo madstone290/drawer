54ec1849079a61cc0169254df0010860c3857c7c
madstone290 <madstone290@gmail.com>
2022-08-28 22:46:08 +0900

    Fix 프로덕션환경에서 정적 파일추가

--- Drawer.Web/Program.cs
+++ Drawer.Web/Program.cs
@@ -17,8 +17,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.HandleArgs(args);
 
-builder.Services.AddBlazoredLocalStorage();
+// dotnet run 명령어 사용시 정적파일을 제공한다.
+builder.WebHost.UseStaticWebAssets();
 
+// 로컬스토리지 사용을 위한 Blazored.LocalStorage 서비스 추가
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices(options =>
